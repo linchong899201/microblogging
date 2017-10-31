3d86485dd0ed788a900c558a67e666e31f3bdf42
linchong899201 <linchong@hotmail.com>
2017-10-31 14:48:50 +0800

    microblogging

---
+++ protected/config/params.php
@@ -0,0 +1,19 @@
+<?php return array(
+	'sendgrid' => array(
+		'username' => '',
+		'password' => ''
+	),
+	'hybridauth' => array(
+	'baseUrl' => '',
+	'base_url' => '',
+	'providers' => array(
+		'Twitter' => array(
+			'enabled' => true,
+			'keys' => array(
+				'key' => '',
+				'secret' => ''
+			)
+		)
+	)
+	)
+);
